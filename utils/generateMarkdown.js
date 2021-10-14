75e44d88117468eec976722a34f12e7f5876b3fe
Chrissie Sparling <gigglewrks@aol.com>
2021-10-13 23:13:01 -0700

    first commit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const inquirer = require("inquirer");
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -11,8 +14,10 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(inquireResponse) {
+  return `# ${inquireResponse.title}
+## ${inquireResponse.description}
+## ${inquireResponse.author}
 
 `;
 }
